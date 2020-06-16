5e97eb822be0124e48a79cbde62500eb0c784048
ajlee21 <alexjlee.21@gmail.com>
2020-06-16 10:13:20 -0400

    update explore data

---
+++ explore_data/nbconverted/0_explore_input_data.py
@@ -0,0 +1,180 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+
+import os
+import sys
+import glob
+import pandas as pd
+import numpy as np
+import random
+import seaborn as sns
+import umap
+from keras.models import load_model
+from sklearn.decomposition import PCA
+import pickle
+
+from plotnine import (ggplot,
+                      labs,  
+                      geom_line, 
+                      geom_point,
+                      geom_errorbar,
+                      aes, 
+                      ggsave, 
+                      theme_bw,
+                      theme,
+                      xlim,
+                      ylim,
+                      facet_wrap,
+                      scale_color_manual,
+                      guides, 
+                      guide_legend,
+                      element_blank,
+                      element_text,
+                      element_rect,
+                      element_line,
+                      coords)
+
+from ponyo import utils
+
+from numpy.random import seed
+randomState = 123
+seed(randomState)
+
+
+# In[2]:
+
+
+# Read in config variables
+base_dir = os.path.abspath(os.path.join(os.getcwd(),"../"))
+
+config_file = os.path.abspath(os.path.join(base_dir,
+                                           "config_human.tsv"))
+params = utils.read_config(config_file)
+
+
+# In[3]:
+
+
+# Load params
+local_dir = params["local_dir"]
+project_id = params['project_id']
+
+
+# In[4]:
+
+
+# Load real template experiment
+template_data_file = params['template_data_file']
+
+# Load metadata file with grouping assignments for samples
+metadata_file = os.path.join(
+    base_dir,
+    "data",
+    "metadata",
+    project_id+"_groups.tsv")
+
+
+# In[5]:
+
+
+# Read data
+template_data = pd.read_csv(
+    template_data_file,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+template_data.head()
+
+
+# In[6]:
+
+
+"""
+# Try different partitions of the data
+smRNA_samples = ["SRR493961",
+                 "SRR493962",
+                 "SRR493963",
+                 "SRR493964",
+                 "SRR493965",
+                 "SRR493966",
+                 "SRR493967",
+                 "SRR493968",
+                 "SRR493969",
+                 "SRR493970",
+                 "SRR493971",
+                 "SRR493972"]
+#template_data = template_data.loc[smRNA_samples]
+template_data = template_data.drop(smRNA_samples)
+print(template_data.shape)
+template_data.head()"""
+
+
+# In[7]:
+
+
+# Read metadata
+metadata = pd.read_csv(
+    metadata_file,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+metadata.head()
+
+
+# In[8]:
+
+
+# PCA encode
+pca = PCA(n_components=2)
+
+model = pca.fit(template_data)
+template_PCAencoded = model.transform(template_data)
+
+template_PCAencoded_df = pd.DataFrame(data=template_PCAencoded ,
+                                         index=template_data.index,
+                                         columns=['1','2'])
+
+
+# In[9]:
+
+
+# Add tumor/normal labels
+template_data_labeled = pd.merge(template_PCAencoded_df, metadata, left_index=True, right_index=True)
+template_data_labeled
+
+
+# In[10]:
+
+
+# Plot
+fig = ggplot(template_data_labeled, aes(x='1', y='2'))
+#fig += ggplot(template_data_labeled[template_data_labeled['source']=='Normal'], aes(x='1', y='2'))
+fig += geom_point(aes(color='source'), alpha=0.7)
+fig += labs(x ='PCA 1',
+            y = 'PCA 2',
+            title = 'PCA template data')
+fig += theme_bw()
+fig += theme(
+    legend_title_align = "center",
+    plot_background=element_rect(fill='white'),
+    legend_key=element_rect(fill='white', colour='white'), 
+    legend_title=element_text(family='sans-serif', size=15),
+    legend_text=element_text(family='sans-serif', size=12),
+    plot_title=element_text(family='sans-serif', size=15),
+    axis_text=element_text(family='sans-serif', size=12),
+    axis_title=element_text(family='sans-serif', size=15)
+    )
+fig += guides(colour=guide_legend(override_aes={'alpha': 1}))
+#fig += ylim(-750000,10)
+#fig += xlim(-1000000,-750000)
+
+print(fig)
+
