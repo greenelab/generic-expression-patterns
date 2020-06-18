b1f89d40afc51671d40b4ae2e978236555349dfd
ajlee21 <alexjlee.21@gmail.com>
2020-06-18 14:16:27 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="generic_modules",
+    version="0.1",
+    description="Functions to support analysis to identify generic expression patterns",
+    url="#",
+    author="Alexandra Lee",
+    author_email="alexjlee.21@gmail.com",
+    license="BSD 3-Clause",
+    packages=["generic_modules"],
+    zip_safe=False,
+)
