917b4251c66f16c5b1a3b22b829f0166d046cc42
Alexandra Lee <alexjlee.21@gmail.com>
2020-06-22 15:34:11 -0400

    Import modules locally (#2)
    * setup file
    
    * rename functions dir to use setup
    
    * update to ignore egg-info created from pip install
    
    * update readme to include added pip install
    
    * update notebook to use modules
    
    * move data files into analysis dir
    
    * rename module dir
    
    * update module name and rerun

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="generic_expression_patterns_modules",
+    version="0.1",
+    description="Functions to support analysis to identify generic expression patterns",
+    url="#",
+    author="Alexandra Lee",
+    author_email="alexjlee.21@gmail.com",
+    license="BSD 3-Clause",
+    packages=["generic_expression_patterns_modules"],
+    zip_safe=False,
+)
