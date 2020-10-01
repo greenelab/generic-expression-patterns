511e3a8aaf1a75d978ac2440fd1ad880eabc1450
dongbohu <dongbo.hu@gmail.com>
2020-10-01 16:00:37 -0400

    Refactoring: human_analysis/2_identify_generic_genes_pathways.ipynb (#23)

--- generic_expression_patterns_modules/DE_analysis.R
+++ generic_expression_patterns_modules/DE_analysis.R
@@ -1,19 +1,14 @@
-## Run this once to setup environment
-## Used R 3.6.3
-#if (!requireNamespace("BiocManager", quietly = TRUE))
-#  install.packages("BiocManager")
+# Load libraries
+library("limma")
+library("DESeq2")
 
-#BiocManager::install("limma")
-
-# library('limma')
-
-get_DE_stats_limma <- function(metadata_file, 
-                               experiment_id, 
+get_DE_stats_limma <- function(metadata_file,
+                               experiment_id,
                                expression_file,
                                data_type,
                                local_dir,
                                run) {
-  
+
   # This function performs DE analysis using expression data in expression_file
   # where samples are grouped based on metadata_file
   #
@@ -36,58 +31,65 @@ get_DE_stats_limma <- function(metadata_file,
   #   Directory to save output files to
   #
   # run: str
-  #   Used as identifier for different simulated experiments 
-  
+  #   Used as identifier for different simulated experiments
+
   # Read in data
   # Note the expression data is transposed to gene x sample in order to run Limma
-  expression_data <- t(as.matrix(read.csv(expression_file, sep="\t", header=TRUE, row.names=1)))
-  metadata <- as.matrix(read.csv(metadata_file, sep="\t", header=TRUE, row.names=1))
-  
-  # NOTE: It make sure the metadata is in the same order 
+  expression_data <- t(
+    as.matrix(
+      read.csv(expression_file, sep="\t", header=TRUE, row.names=1)
+    )
+  )
+  metadata <- as.matrix(
+    read.csv(metadata_file, sep="\t", header=TRUE, row.names=1)
+  )
+
+  # NOTE: It make sure the metadata is in the same order
   # as the column names of the expression matrix.
   group <- interaction(metadata[,1])
-  
+
   mm <- model.matrix(~0 + group)
-  
+
   ## DEGs of simulated data
   # lmFit expects input array to have structure: gene x sample
   # lmFit fits a linear model using weighted least squares for each gene:
   fit <- lmFit(expression_data, mm)
-  
-  # Comparisons between groups (log fold-changes) are obtained as contrasts of these fitted linear models:
+
+  # Comparisons between groups (log fold-changes) are obtained as contrasts of
+  # these fitted linear models:
   # Samples are grouped based on experimental condition
   # The variability of gene expression is compared between these groups
   contr <- makeContrasts(group2 - group1, levels = colnames(coef(fit)))
-  
+
   # Estimate contrast for each gene
   tmp <- contrasts.fit(fit, contr)
-  
-  # Empirical Bayes smoothing of standard errors (shrinks standard errors 
+
+  # Empirical Bayes smoothing of standard errors (shrinks standard errors
   # that are much larger or smaller than those from other genes towards the average standard error)
   tmp <- eBayes(tmp)
-  
+
   # Get significant DEGs
   top.table <- topTable(tmp, sort.by = "P", n = Inf)
   all_genes <-  as.data.frame(top.table)
-  
+
   # Find all DEGs based on adjusted p-value cutoff
   threshold <- 0.001
   num_sign_DEGs <- all_genes[all_genes[,'adj.P.Val']<threshold & abs(all_genes[,'logFC'])>1,]
-  
+
   # Save summary statistics of DEGs
   if (data_type == "template") {
     out_file = paste(local_dir, "DE_stats/DE_stats_template_data_", experiment_id,"_", run, ".txt", sep="")
   } else if (data_type == "simulated") {
     out_file = paste(local_dir, "DE_stats/DE_stats_simulated_data_", experiment_id,"_", run, ".txt", sep="")
-  }  
+  }
   write.table(all_genes, file = out_file, row.names = T, sep = "\t", quote = F)
-  
+
   return(nrow(num_sign_DEGs))
-  
+
 }
 
-get_DE_stats_DESeq <- function(metadata_file, 
-                               experiment_id, 
+get_DE_stats_DESeq <- function(metadata_file,
+                               experiment_id,
                                expression_file,
                                data_type,
                                local_dir,
@@ -114,7 +116,7 @@ get_DE_stats_DESeq <- function(metadata_file,
   #   Directory to save output files to
   #
   # run: str
-  #   Used as identifier for different simulated experiments 
+  #   Used as identifier for different simulated experiments
 
   expression_data <- t(as.matrix(read.csv(expression_file, sep="\t", header=TRUE, row.names=1)))
   metadata <- as.matrix(read.csv(metadata_file, sep="\t", header=TRUE, row.names=1))
@@ -129,7 +131,7 @@ get_DE_stats_DESeq <- function(metadata_file,
   #print(head(expression_data))
 
   ddset <- DESeqDataSetFromMatrix(expression_data, colData=metadata, design = ~group)
-  
+
   deseq_object <- DESeq(ddset)
 
   deseq_results <- results(deseq_object)
@@ -141,6 +143,6 @@ get_DE_stats_DESeq <- function(metadata_file,
     out_file = paste(local_dir, "DE_stats/DE_stats_template_data_", experiment_id,"_", run, ".txt", sep="")
   } else if (data_type == "simulated") {
     out_file = paste(local_dir, "DE_stats/DE_stats_simulated_data_", experiment_id,"_", run, ".txt", sep="")
-  }  
+  }
   write.table(deseq_results_df, file = out_file, row.names = T, sep = "\t", quote = F)
 }
