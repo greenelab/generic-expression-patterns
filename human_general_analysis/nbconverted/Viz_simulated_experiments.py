8cf9d458206f289d0b1602db1ac5946558449d53
ajlee21 <alexjlee.21@gmail.com>
2020-12-10 09:34:45 -0500

    update volcano plots - rm annotations, add comments

--- human_general_analysis/nbconverted/Viz_simulated_experiments.py
+++ human_general_analysis/nbconverted/Viz_simulated_experiments.py
@@ -3,7 +3,7 @@
 
 # ## Visualize simulated experiment
 # 
-# This notebook will make volcano plots of 3 representative simulated experiments to demonstrate what the simulation approach creates from a biological perspective (i.e. experiments created contain different sets of DEGs that are related)
+# This notebook will make volcano plots of 3 representative simulated experiments to demonstrate what the simulation approach creates from a biological perspective (i.e. experiments created contain a similar experiment structure but different biological patterns)
 
 # In[1]:
 
@@ -44,19 +44,79 @@ simulated_DE_stats_dir = os.path.join(
     local_dir, 
     "DE_stats"
 )
+template_DE_stats_filename = os.path.join(
+    local_dir, 
+    "DE_stats",
+    f"DE_stats_template_data_{project_id}_real.txt"
+    
+)
 
 
 # In[4]:
 
 
-out_filename = "example_simulated_volcano.svg"
-
-
-# In[5]:
-
-
 # Plot volcano plots
-def make_volcano_plot(
+def make_volcano_plot_template(
+    template_DE_stats_filename,
+    project_id,
+    out_filename
+):
+    
+    # Read template DE stats
+    template_DE_stats_df = pd.read_csv(
+        template_DE_stats_filename,
+        sep="\t",
+        index_col=0,
+        header=0
+    )
+    
+    # Take -log10 of adjusted p-value
+    template_DE_stats_df["padj_log10"] = -np.log10(template_DE_stats_df["padj"])
+
+    # Label DEGs by traditional criteria
+    # log2FC > 1
+    # padj < 0.05
+    template_DE_stats_df["gene group"] = "none"
+    template_DE_stats_df.loc[(abs(template_DE_stats_df["log2FoldChange"])>1) &
+                          (template_DE_stats_df["padj"] <0.05),
+                              "gene group"
+                         ] = "DEG"
+
+    # Plot
+    colors = ["lightgrey", "#2c7fb8"]
+
+    f = sns.scatterplot(
+       data=template_DE_stats_df,
+        x="log2FoldChange",
+        y="padj_log10",
+        hue="gene group",
+        hue_order=["none", "DEG"],
+        style="gene group",
+        markers={
+            "none": ".",
+            "DEG": "o",
+        },
+        palette=colors,
+        linewidth=0,
+        alpha=0.5,
+        )
+    
+    f.set_xlabel("log2 Fold Change", fontsize=14, fontname="Verdana")
+    f.set_ylabel("-log10(FDR adjusted p-value)", fontsize=14, fontname="Verdana")
+    f.set_title(f"Template experiment ({project_id})", fontsize=16, fontname="Verdana")
+    
+    # Save plot
+    f.figure.savefig(
+        out_filename,
+        format="svg",
+        bbox_inches="tight",
+        transparent=True,
+        pad_inches=0,
+        dpi=300,
+    )
+        
+    
+def make_volcano_plot_simulated(
     simulated_DE_stats_dir,
     project_id,
     num_examples,
@@ -91,12 +151,6 @@ def make_volcano_plot(
                               (simulated_DE_stats_df["padj"] <0.05),
                                   "gene group"
                              ] = "DEG"
-
-        # Gene annotations
-        annotate_genes_by_pval = list(simulated_DE_stats_df.nsmallest(5,"padj").index)
-        annotate_genes_by_logFC = list(simulated_DE_stats_df.loc[abs(simulated_DE_stats_df["log2FoldChange"])>1].index)
-        annotate_genes = list(set(annotate_genes_by_pval).intersection(annotate_genes_by_logFC))
-        simulated_DE_stats_df.loc[annotate_genes, "annotate"] = annotate_genes
         
         # Plot
         colors = ["lightgrey", "#2c7fb8"]
@@ -122,41 +176,7 @@ def make_volcano_plot(
         axes[i].set_ylabel("")
         axes[i].set_xlabel("")
         
-        # Annotate DEGs that are at extremes
-        if i==0:
-            counter=0
-            # Alternate annotations to be left and right of point due to over-crowding of points
-            for gene in simulated_DE_stats_df.dropna().index:
-                if counter%2 ==0:
-                    f.text(
-                        simulated_DE_stats_df.loc[gene,"log2FoldChange"]+0.4,
-                        simulated_DE_stats_df.loc[gene,"padj_log10"],
-                        simulated_DE_stats_df.loc[gene,"annotate"],
-                        horizontalalignment='left',
-                        size='medium', 
-                        color='black',
-                    )
-                else:
-                    f.text(
-                        simulated_DE_stats_df.loc[gene,"log2FoldChange"]-3.8,
-                        simulated_DE_stats_df.loc[gene,"padj_log10"],
-                        simulated_DE_stats_df.loc[gene,"annotate"],
-                        horizontalalignment='left',
-                        size='medium', 
-                        color='black',
-                    )
-                counter +=1
-                    
-        else:
-            for gene in simulated_DE_stats_df.dropna().index:
-                f.text(
-                    simulated_DE_stats_df.loc[gene,"log2FoldChange"]+0.4,
-                    simulated_DE_stats_df.loc[gene,"padj_log10"],
-                    simulated_DE_stats_df.loc[gene,"annotate"],
-                    horizontalalignment='left',
-                    size='medium', 
-                    color='black',
-                )
+        
     fig.legend(labels=["DEGs", "other genes"], loc='center right')
     fig.text(0.5, 0.0, "log2 Fold Change",ha="center", fontsize=14, fontname="Verdana")
     fig.text(0.08, 0.5, "-log10(FDR adjusted p-value)", va="center", rotation="vertical", fontsize=14, fontname="Verdana")
@@ -173,13 +193,23 @@ def make_volcano_plot(
     )
 
 
+# In[5]:
+
+
+# Plot template volcano
+out_filename = "template_volcano.svg"
+make_volcano_plot_template(template_DE_stats_filename, project_id, out_filename)
+
+
 # In[6]:
 
 
-make_volcano_plot(simulated_DE_stats_dir, project_id, 3, out_filename)
+# Plot simulated volcanos
+out_filename = "example_simulated_volcano.svg"
+make_volcano_plot_simulated(simulated_DE_stats_dir, project_id, 3, out_filename)
 
 
-# From left to right:
-# * DEGs in the first simulated experiment are related to cell growth, cell cycle regulation, cell-to-cell signaling
-# * DEGs in the second simulated experiment are related to cell fate and cell differentiation
-# * DEGs in the third simulated experiment are related to ion channel gating within neurons, muscle heart development
+# **Takeaway:**
+# * Simulated experiments have DEGs with a similar distribution of effect size changes (i.e. our simulation preserve the experimental design, so the "changes" are maintained)
+# * However, most DEGs in the simulated experiments are down-regulated compared to the template experiment where genes are equally split between up-regulated and down-regulated. This indicates the different biological patterns that are generated by the simulated (i.e. the simulated experiments are different experiments compared to the template). Furthermore, there are inter-simulated experiments differences in the volcano plot, indicating that these are distinct new experiments. 
+# * In both template and simulated experiments, DEGs are determines by p-value more than the log2 fold change (i.e minimally changed genes are signficant). This might suggest that very few genes were changed in this experiment? (Need to think about this more)
