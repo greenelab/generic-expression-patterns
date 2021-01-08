502b42fe4d0b12868a69df05462fbac2571c6193
ajlee21 <alexjlee.21@gmail.com>
2021-01-08 09:23:28 -0500

    clean up notebook comments

--- compare_experiments/nbconverted/compare_generic_genes.py
+++ compare_experiments/nbconverted/compare_generic_genes.py
@@ -3,7 +3,7 @@
 
 # # Compare generic genes
 # 
-# The goal of this notebook is to compare the generic genes found using 2 different recount2 template experiments to determine ____
+# The goal of this notebook is to compare the generic genes found using 2 different recount2 template experiments.
 
 # In[1]:
 
@@ -70,13 +70,21 @@ gene_ranking2 = gene_ranking_summary2["Rank (simulated)"].rename("Rank 2")
 gene_ranking_combined = pd.concat([gene_ranking1, gene_ranking2], axis=1)
 
 
-# In[16]:
+# In[6]:
 
 
-gene_ranking_combined.loc[gene_ranking_combined.isna().any(axis=1)]
+print(gene_ranking_combined.shape)
+gene_ranking_combined.head()
 
 
 # In[7]:
+
+
+# Check for NAs
+gene_ranking_combined[pd.isnull(gene_ranking_combined).any(axis=1)]
+
+
+# In[8]:
 
 
 # Plot correlation between ranking
@@ -92,53 +100,111 @@ fig.set_axis_labels(
     f"Ranking in {project_id1}", f"Ranking in {project_id2}", fontsize=14, fontname="Verdana"
 )
 
-"""fig.savefig(
+output_figure_filename = "concordance_between_recount2_templates.svg"
+fig.savefig(
     output_figure_filename,
     format="svg",
     bbox_inches="tight",
     transparent=True,
     pad_inches=0,
     dpi=300,
-)"""
-
-
-# In[8]:
-
-
-print(gene_ranking1.shape)
-gene_ranking_summary1.loc["A1CF"]
-
-
-# In[9]:
-
-
-print(gene_ranking2.shape)
-gene_ranking_summary2.loc["A1CF"]
+)
 
 
 # **Takeaway:**
 # 
 # * Looks like there is good concordance between highly ranked genes (i.e. generic genes)
-# * Are the gene rankings robust? We expect that there are some genes that are generally generic and there are also genes that are generic within the given context.
+# * In general, we expect that there are some genes that are generally generic (i.e. those that are concordant between these two experiments) and there are also genes that are generic within the given context of the specific experiment.
 
 # ## Examine gene expression data
 
-# In[10]:
+# In[9]:
 
 
+# Read expression data
 template_1 = pd.read_csv(template_filename1, sep="\t", index_col=0, header=0)
 template_2 = pd.read_csv(template_filename2, sep="\t", index_col=0, header=0)
 
 
-# In[17]:
+# In[10]:
 
 
-sns.distplot(template_1.mean(), kde=False)
-sns.distplot(template_2.mean(), kde=False)
+# Get concordance genes
+concordant_genes = list(gene_ranking_combined[(gene_ranking_combined["Rank 1"]>15000) &
+                                         (gene_ranking_combined["Rank 2"]>15000)].index)
+
+# Get disconcordant genes
+discordant_genes = set(gene_ranking_combined.index).difference(concordant_genes)
+
+
+# In[11]:
+
+
+# Distribution of concordant genes in template experiment 1
+template1_mean = template_1.mean()
+
+print("Percent concordant genes with 0 expression in template 1:", 
+      len(template1_mean[concordant_genes].loc[template1_mean[concordant_genes]==0])/len(template1_mean[concordant_genes]))
+
+print("Percent nonzero concordant genes in template 1:", 
+      len(template1_mean[concordant_genes].loc[(template1_mean[concordant_genes]>0) &
+                                           (template1_mean[concordant_genes]<1000)])/len(template1_mean[concordant_genes]))
+sns.distplot(template_1.mean()[concordant_genes], kde=False)
+
+
+# In[12]:
+
+
+# Distribution of concordant genes in template experiment 2
+template2_mean = template_2.mean()
+print("Percent concordant genes with 0 expression in template 2:",
+      len(template2_mean[concordant_genes].loc[template2_mean[concordant_genes]==0])/len(template2_mean[concordant_genes]))
+
+print("Percent nonzero concordant genes in template 2:",
+      len(template2_mean[concordant_genes].loc[(template2_mean[concordant_genes]>0) &
+                                                (template2_mean[concordant_genes]<1000)])/len(template2_mean[concordant_genes]))
+
+# There are more 0 expressed genes in this template experiment
+sns.distplot(template_2.mean()[concordant_genes], kde=False)
+
+
+# In[13]:
+
+
+# Distribution of discordant gense in template experiment 1
+template1_mean = template_1.mean()
+
+print("Percent discordant genes with 0 expression in template 1:", 
+      len(template1_mean[discordant_genes].loc[template1_mean[discordant_genes]==0])/len(template1_mean[discordant_genes]))
+
+print("Percent nonzero discordant genes in template 1:", 
+      len(template1_mean[discordant_genes].loc[(template1_mean[discordant_genes]>0) &
+                                           (template1_mean[discordant_genes]<1000)])/len(template1_mean[discordant_genes]))
+
+print(len(template1_mean[discordant_genes].loc[template1_mean[discordant_genes]>0])/len(template1_mean[discordant_genes]))
+sns.distplot(template_1.mean()[discordant_genes], kde=False)
+
+
+# In[14]:
+
+
+# Distribution of discordant genes in template experiment 2
+template2_mean = template_2.mean()
+
+print("Percent discordant genes with 0 expression in template 2:",
+      len(template2_mean[discordant_genes].loc[template2_mean[discordant_genes]==0])/len(template2_mean[discordant_genes]))
+
+print("Percent nonzero discordant genes in template 2:",
+      len(template2_mean[discordant_genes].loc[(template2_mean[discordant_genes]>0) &
+                                                (template2_mean[discordant_genes]<1000)])/len(template2_mean[discordant_genes]))
+
+sns.distplot(template_2.mean()[discordant_genes], kde=False)
 
 
 # **Takeaway:**
 # 
-# Are there differences in the distribution of mean gene expression? Doesn't look like there are
+# Doesn't appear to be much of a difference between the distribution of average gene expression values for these two experiments. 
 # 
-# Do we expect that differences in gene expression in the template experiment will yeild different generic genes? Theoretically, if a gene is lowly expressed in the context of template experiment 1 it shouldn't be found to be generic. But this same gene could be found to be generic in the context of template experiment 2 if it is more expressed.
+# Theoretically, I would expect the scenario where a gene is lowly expressed in the context of template experiment 1 and therefore not found to be generic. But this same gene could be found to be generic in the context of template experiment 2 if it is more expressed. Its possible that differences in gene expression distribution can change which genes are found to be generic given that the simulation is producing experiments with a similar context. 
+# 
+# In this case, despite having similar gene expression distributions there are still many differences in gene ranking. This suggests to me that level of gene expression activity doesn't matter as much as the overall patterns perhaps. 
