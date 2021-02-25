ead20908ea574144de5f875d005a571f3e0b0066
Jake Crawford <jjc2718@gmail.com>
2021-02-25 17:17:22 -0500

    update documentation and add a few more asserts

--- network_analysis/sample_nodes.py
+++ network_analysis/sample_nodes.py
@@ -2,17 +2,40 @@ import string
 
 import numpy as np
 
-def sample_degree_matched(nodes,
-                          degrees,
-                          is_generic,
-                          sort=False,
-                          num_bins=10):
+def sample_degree_matched(nodes, degrees, is_generic, num_bins=10):
 
-    # NOTE: nodes, degrees, is_generic are assumed to be sorted
-    # by ascending degree
+    """
+    Sample nodes from a network, with rough degree matching to is_generic.
 
-    assert nodes.shape[0] == degrees.shape[0]
-    assert nodes.shape[0] == is_generic.shape[0]
+    Essentially, this works as follows:
+      * Split array into num_bins bins, of approximately equal size
+        (that is, all the nodes in each bin have approximately equal degree)
+      * For each generic node, sample a non-generic node from the same bin,
+        without replacement
+        (if there are more generic nodes than non-generic nodes in a bin,
+         an error will be thrown at this step)
+
+    Arguments
+    ---------
+    nodes (np.array): node names
+    degrees (np.array): node degrees
+    is_generic (np.array): boolean array, nodes are or are not generic
+    num_bins (int): number of bins to split node list into
+
+    Returns
+    -------
+    sampled_nodes (np.array): names of sampled nodes
+    sampled_degrees (np.array): degrees of sampled arrays
+    bin_ixs (np.array): array of arrays containing indexes in each bin
+
+    NOTE: nodes, degrees, is_generic are assumed to be sorted by ascending
+    degree when passed into the function.
+    """
+    # make sure all arrays have equal shape
+    assert nodes.shape == degrees.shape
+    assert nodes.shape == is_generic.shape
+    # make sure degree array is sorted
+    assert np.all(np.diff(degrees) >= 0)
 
     # get split indexes
     bin_ixs = np.array_split(np.arange(nodes.shape[0]), num_bins)
@@ -26,11 +49,11 @@ def sample_degree_matched(nodes,
         # indices in the bin that are not generic genes
         bin_non_generic = bin_ix[~is_generic[bin_ix].astype('bool')]
 
-        # this should always be the case since there are far fewer
-        # generic genes than non-generic genes
+        # this should always be the case in our experiments, since there are
+        # far fewer generic genes than non-generic genes
         # if it's not we need to have bigger bins or otherwise rethink,
         # since sampling w/o replacement won't work
-        assert bin_generic.shape[0] < bin_non_generic.shape[0]
+        assert bin_generic.shape[0] <= bin_non_generic.shape[0]
 
         # sample as many nodes from the non-generic genes in the bin
         # as there are generic genes in the bin, without replacement
@@ -48,21 +71,16 @@ if __name__ == '__main__':
     degrees = np.random.geometric(p=0.2, size=size)
     nodes = np.array(list(string.printable)[:size])
     # sort nodes and degrees by degree
-    print(nodes)
-    print(degrees)
     nodes, degrees = zip(*sorted(
         zip(nodes, degrees),
         key=lambda x: x[1]
     ))
     nodes = np.array(nodes)
     degrees = np.array(degrees)
-    print(nodes)
-    print(degrees)
     is_generic = np.random.choice([0, 1], size=size, p=[0.8, 0.2])
-    print(is_generic)
-    s_nodes, s_degrees, _ = sample_degree_matched(nodes, degrees, is_generic, sort=True)
     print(nodes[is_generic.astype('bool')])
     print(degrees[is_generic.astype('bool')])
+    s_nodes, s_degrees, _ = sample_degree_matched(nodes, degrees, is_generic)
     print(s_nodes)
     print(s_degrees)
 
