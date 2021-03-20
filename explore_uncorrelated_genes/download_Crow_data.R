10e8a04a615cb8f0c42afb55e3829646c86f16ea
ajlee21 <alexjlee.21@gmail.com>
2021-03-20 03:21:20 -0400

    update download script and metadata file for errd dataset ids

--- explore_uncorrelated_genes/download_Crow_data.R
+++ explore_uncorrelated_genes/download_Crow_data.R
@@ -7,20 +7,21 @@
 # BLAS:   /usr/lib/x86_64-linux-gnu/blas/libblas.so.3.7.1
 # LAPACK: /usr/lib/x86_64-linux-gnu/lapack/liblapack.so.3.7.1
 #
-# locale:
+#locale:
 #  [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C               LC_TIME=en_US.UTF-8        LC_COLLATE=en_US.UTF-8
 # [5] LC_MONETARY=en_US.UTF-8    LC_MESSAGES=en_US.UTF-8    LC_PAPER=en_US.UTF-8       LC_NAME=C
-# [9] LC_ADDRESS=C               LC_TELEPHONE=C             LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C
+#[9] LC_ADDRESS=C               LC_TELEPHONE=C             LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C
 #
-# attached base packages:
+#attached base packages:
 #  [1] stats     graphics  grDevices utils     datasets  methods   base
 #
-#loaded via a namespace (and not attached):
-#  [1] Rcpp_1.0.6        rstudioapi_0.13   magrittr_2.0.1    usethis_2.0.1     devtools_2.3.2    pkgload_1.1.0     R6_2.5.0
-# [8] rlang_0.4.10      plyr_1.8.6        tools_3.6.3       pkgbuild_1.2.0    sessioninfo_1.1.1 cli_2.3.1         withr_2.4.1
-# [15] ellipsis_0.3.1    remotes_2.2.0     assertthat_0.2.1  digest_0.6.27     rprojroot_2.0.2   lifecycle_1.0.0   crayon_1.4.1
-# [22] processx_3.4.5    purrr_0.3.4       callr_3.5.1       fs_1.5.0          ps_1.5.0          curl_4.3          testthat_3.0.1
-# [29] memoise_2.0.0     glue_1.4.2        compiler_3.6.3    desc_1.2.0        prettyunits_1.1.1
+# loaded via a namespace (and not attached):
+#  [1] rstudioapi_0.13     magrittr_2.0.1      hms_1.0.0           tidyselect_1.1.0    R6_2.5.0            rlang_0.4.10
+# [7] fansi_0.4.2         blob_1.2.1          httr_1.4.2          dplyr_1.0.5         tools_3.6.3         R.oo_1.24.0
+# [13] utf8_1.2.1          DBI_1.1.0           gemmaAPI_2.0.3.9000 cli_2.3.1           ellipsis_0.3.1      assertthat_0.2.1
+# [19] tibble_3.1.0        lifecycle_1.0.0     crayon_1.4.1        purrr_0.3.4         readr_1.4.0         vctrs_0.3.6
+# [25] R.utils_2.10.1      curl_4.3            glue_1.4.2          compiler_3.6.3      pillar_1.5.1        generics_0.1.0
+# [31] R.methodsS3_1.8.1   pkgconfig_2.0.3
 
 # This script is downloading data associated with Crow et. al. paper
 # Following instructions from: https://github.com/PavlidisLab/gemmaAPI.R
@@ -34,6 +35,9 @@ install.packages('devtools')
 devtools::install_github('PavlidisLab/gemmaAPI.R')
 
 # Read in supplementary table containing experiment ids to download
+# Note manually adjustments:
+# Change name: GSE11512-Human --> GSE11512.1
+# Remove "GSE25119"; "GSE33903"; "GSE36809", "GSE7214"
 supplementary_filename <- "/home/alexandra/Documents/Repos/generic-expression-patterns/explore_uncorrelated_genes/pnas.1802973116.sd01.csv"
 metadata <- as.matrix(read.csv(supplementary_filename, header=TRUE, row.names=1))
 
@@ -74,15 +78,30 @@ for (i in 1:length(gse_ids)){
     all_data <- data_final
   }
   else{
-    shared_gene_ids <- intersect(names(all_data), names(data_final))
-    all_data <- rbind(all_data[, shared_gene_ids], data_final[, shared_gene_ids])
+    #shared_gene_ids <- intersect(names(all_data), names(data_final))
+    #all_data_intersect <- rbind(all_data[, shared_gene_ids], data_final[, shared_gene_ids])
+    all_data <- dplyr::bind_rows(all_data, data_final)
   }
 }
 
 # Save
-write.table(all_data, "/home/alexandra/Documents/Data/Generic_expression_patterns/Crow_expression_data.tsv", sep="\t", row.names=TRUE, col.names=TRUE)
+write.table(all_data, "/home/alexandra/Documents/Data/Generic_expression_patterns/Crow_expression_data_union.tsv", sep="\t", row.names=TRUE, col.names=TRUE)
 
+# tmp call
+#result_pt1_filename <- "/home/alexandra/Documents/Data/Generic_expression_patterns/Crow_expression_data_part1.tsv"
+#result_pt2_filename <- "/home/alexandra/Documents/Data/Generic_expression_patterns/Crow_expression_data_part2.tsv"
+#result_pt3_filename <- "/home/alexandra/Documents/Data/Generic_expression_patterns/Crow_expression_data_part3.tsv"
 
+#result_part1 <- as.data.frame(read.csv(result_pt1_filename, sep="\t", header=TRUE, row.names=1))
+#result_part2 <- as.data.frame(read.csv(result_pt2_filename, sep="\t", header=TRUE, row.names=1))
+#result_part3 <- as.data.frame(read.csv(result_pt3_filename, sep="\t", header=TRUE, row.names=1))
+
+#shared_gene_ids <- intersect(names(result_part1), names(result_part2))
+#shared_gene_ids_final <- intersect(shared_gene_ids, names(result_part3))
+#all_data_new <- rbind(result_part1[, shared_gene_ids_final], result_part2[, shared_gene_ids_final])
+#all_data_new <- rbind(all_data_new[, shared_gene_ids_final], result_part3[, shared_gene_ids_final])
+
+#write.table(all_data_new, "/home/alexandra/Documents/Data/Generic_expression_patterns/Crow_expression_data.tsv", sep="\t", row.names=TRUE, col.names=TRUE)
 #----- Scratch work -------
 # To confirm the logic of the above script runs as expected
 #data1 <- as.data.frame(gemmaAPI::datasetInfo(gse_ids[1], request='data', filter=FALSE))
