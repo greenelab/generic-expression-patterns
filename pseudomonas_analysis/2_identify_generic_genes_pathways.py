161e3eaed8b55a4073c9cb77189d6dccafd5b701
ajlee21 <alexjlee.21@gmail.com>
2021-04-19 16:53:46 -0400

    fix Pa correlation plot

--- pseudomonas_analysis/2_identify_generic_genes_pathways.py
+++ pseudomonas_analysis/2_identify_generic_genes_pathways.py
@@ -98,6 +98,9 @@ metadata_filename = os.path.join(
 
 # Load pickled file
 scaler = pickle.load(open(scaler_filename, "rb"))
+
+# Percentile threshold to identify generic genes
+percentile_threshold = 80.0
 # -
 
 # Output files
@@ -117,7 +120,7 @@ pathway_summary_filename = os.path.join(
 # 4. Decode the samples. This results in a new experiment
 # 5. Repeat steps 1-4 to get multiple simulated experiments
 
-# Simulate multiple experiments
+"""# Simulate multiple experiments
 # This step creates the following files in "<local_dir>/pseudo_experiment/" directory:
 #   - selected_simulated_data_SRP012656_<n>.txt
 #   - selected_simulated_encoded_data_SRP012656_<n>.txt
@@ -135,15 +138,14 @@ for run_id in range(num_runs):
         local_dir,
         base_dir,
         run_id,
-    )
+    )"""
 
 # ### Process template and simulated data
 #
 # * Remove samples not required for comparison.
 # * Make sure ordering of samples matches metadata for proper comparison
 
-# +
-if not os.path.exists(sample_id_metadata_filename):
+"""if not os.path.exists(sample_id_metadata_filename):
     sample_id_metadata_filename = None
 
 stats.process_samples_for_limma(
@@ -162,7 +164,7 @@ for i in range(num_runs):
         metadata_filename,
         None,
         sample_id_metadata_filename,
-    )
+    )"""
 
 # +
 # Quick check
@@ -301,13 +303,15 @@ shared_ranking.head()
 # are also common in GAPE? What is the probability of drawing
 # k or more concordant genes?
 num_GAPE_genes = shared_ranking.shape[0]
-num_generic_GAPE_genes = shared_ranking[shared_ranking[ref_rank_col] >= 60.0].shape[0]
+num_generic_GAPE_genes = shared_ranking[
+    shared_ranking[ref_rank_col] >= percentile_threshold
+].shape[0]
 num_generic_SOPHIE_genes = shared_ranking[
-    shared_ranking["Percentile (simulated)"] >= 80.0
+    shared_ranking["Percentile (simulated)"] >= percentile_threshold
 ].shape[0]
 num_concordant_generic_genes = shared_ranking[
-    (shared_ranking[ref_rank_col] >= 60.0)
-    & (shared_ranking["Percentile (simulated)"] >= 80.0)
+    (shared_ranking[ref_rank_col] >= percentile_threshold)
+    & (shared_ranking["Percentile (simulated)"] >= percentile_threshold)
 ].shape[0]
 
 print(num_GAPE_genes)
